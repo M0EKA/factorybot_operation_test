f8f027507c8630dc12154432aa39a73b55cbd55c
M0EKA <momoseaoi.1810@gmail.com>
2022-06-30 11:55:02 +0900

    scaffold等のファイル生成時の生成されるファイルの設定
    test系のファイルを自動生成しないこともできるが、フォーマットの参考にできるので今回は自動生成する設定のままやる

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module FactorybotOperationTest
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Tokyo"
+    config.generators do |g|
+      g.assets false # cssとかJSとかのファイルの生成を無効化
+      g.test_framework :rspec # 生成されるtest系のファイルのtestフレームワークをrspecに指定
+      g.helper false # helper無視
+      g.jbuilder false # jbuilder … rails にAPIを作れるものがdefaultで設定されている。それを無視する。
+    end
   end
 end
